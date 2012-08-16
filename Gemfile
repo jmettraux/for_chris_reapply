dd5fda2b4c702bc28ce147101e683c4b480e722e
John Mettraux <jmettraux@gmail.com>
2012-08-16 09:14:12 +0900

    initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+
+source :rubygems
+
+gem 'yajl-ruby', '1.0.0', :require => 'yajl'
+
+gem 'ruote', :git => 'git://github.com/jmettraux/ruote.git', :ref => 'd69981252e3d9f4f40ee09777319fcd15fa62e7e'
+gem 'ruote-redis', :git => 'git://github.com/jmettraux/ruote-redis.git', :ref => '8b5e847874fb13753541e197de806ba312e739c8'
+
---
+++ case.rb
@@ -0,0 +1,68 @@
+
+require 'pp'
+
+require 'rufus-json/automatic'
+require 'ruote'
+require 'ruote-redis'
+
+
+#
+# ruote conf
+
+ruote = Ruote::Dashboard.new(
+  Ruote::Worker.new(
+    Ruote::Redis::Storage.new('db' => 14, 'thread_safe' => true)))
+
+ruote.noisy = ENV['NOISY'] == 'true'
+
+ruote.storage.purge!
+
+#
+# actual case
+
+# ---8<---
+# define
+#   subprocess tasklet
+#
+# define tasklet
+#   cursor
+#     subprocess work  <--- here
+#
+# define work
+#   cursor tag: work2
+# --->8---
+
+pdef = Ruote.define do
+  tasklet
+  define 'tasklet' do
+    cursor do
+      work
+    end
+  end
+  define 'work' do
+    cursor :tag => 'work2' do
+      stall
+    end
+  end
+end
+
+wfid = ruote.launch(pdef)
+
+sleep 1.5
+
+pp ruote.ps(wfid).expressions.map { |e|
+  [ e.name, e.fei.expid, e.fei.subid, e.state ].join(' ')
+}
+
+t = ruote.ps(wfid).expressions.find { |e| e.fei.expid == '0_0_0_0' }
+
+puts "re-applying: " + [ t.name, t.fei.expid, t.fei.subid, t.state ].join(' ')
+
+ruote.re_apply(t)
+
+sleep 1.5
+
+pp ruote.ps(wfid).expressions.map { |e|
+  [ e.name, e.fei.expid, e.fei.subid, e.state ].join(' ')
+}
+
