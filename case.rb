2b2362158c5c4d5df79d2d2d45b1837b8231ee2c
John Mettraux <jmettraux@gmail.com>
2012-08-16 09:18:50 +0900

    add output aeration

--- case.rb
+++ case.rb
@@ -51,19 +51,24 @@ wfid = ruote.launch(pdef)
 
 sleep 0.7
 
+puts
 pp ruote.ps(wfid).expressions.map { |e|
   [ e.name, e.fei.expid, e.fei.subid, e.state ].join(' ')
 }
 
 t = ruote.ps(wfid).expressions.find { |e| e.fei.expid == '0_0_0_0' }
 
+puts
 puts "re-applying: " + [ t.name, t.fei.expid, t.fei.subid, t.state ].join(' ')
 
 ruote.re_apply(t)
 
 sleep 0.7
 
+puts
 pp ruote.ps(wfid).expressions.map { |e|
   [ e.name, e.fei.expid, e.fei.subid, e.state ].join(' ')
 }
+
+puts
 
